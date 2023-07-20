46a8c3eabfa82371b91ee40f5f2ba258388d256c
Vinícius Diniz <105671764+ViniciusDX@users.noreply.github.com>
2023-07-19 22:08:27 -0300

    formGrup
    formGrup

--- projeto-kaspper/src/app/app.module.ts
+++ projeto-kaspper/src/app/app.module.ts
@@ -6,17 +6,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './component/admin/footer/footer.component';
 import { HeaderComponent } from './component/admin/header/header.component';
+import { LoginComponent } from './component/login/login.component';
 
 @NgModule({
   declarations: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HeaderComponent,
     FooterComponent,
+    LoginComponent,
   ],
-  providers: [],
-  bootstrap: [AppComponent],
 })
 export class AppModule {}
